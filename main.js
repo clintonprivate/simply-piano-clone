9a3db0a6b2bb171a5d187a410622c1cb0a9192ef
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2024-07-18 02:48:29 +0200

    Better element positions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var stageType = "newnote";
 var staffNotes = ["E", "F", "G", "A", "B", "C", "D", "E", "F"];
 var noteToPlay = "E";
+var notesToLearn = ["A3", "B3", "E4", "F#3", "G#3", "C#4", "E#4"]
 var highestStaffLineHeight = document.getElementById("staff").offsetTop;
 var lowestStaffLineHeight = document.getElementById("staff").offsetTop + document.getElementById("staff").offsetHeight;
 
