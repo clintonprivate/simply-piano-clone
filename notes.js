223afb0aa184349d07238226664f74d3f87d3b23
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2024-07-19 04:24:19 +0200

    Create next stage function working

--- notes.js
+++ notes.js
@@ -1,9 +1,42 @@
+function createNextStage() {
+    var stage = [];
+    if (currentStage == 1) {
+        stage = Array(3).fill(notesToLearn[0]);
+        learnedNotes.push(notesToLearn[0]);
+    } else if (currentStage == 2) {
+        stage = Array(3).fill(notesToLearn[1]);
+        learnedNotes.push(notesToLearn[1]);
+    } else if (learnedNotes.length < notesToLearn.length) {
+        if (currentStage % 2 == 0) {
+            stage = Array(3).fill(notesToLearn[learnedNotes.length]);
+            learnedNotes.push(notesToLearn[learnedNotes.length]);
+        }
+        else {
+            var latestLearnedNote = learnedNotes[learnedNotes.length-1];
+            stage.push(latestLearnedNote);
+            for (let i = 0; i < 4; i++) {
+                const randomIndex = Math.floor(Math.random() * learnedNotes.length);
+                stage.push(learnedNotes[randomIndex]);
+            }
+            stage = shuffleArray(stage);
+        }
+    }
+    else {
+        stage = ["play"];
+    }
+    currentStage++;
+    return stage;
+}
+
+function generateNewNotes() {
+    var nextStage = createNextStage();
+}
+
 function setNotePosition() {
     var lowestStaffNote = ["E", 4];
     var lowestStaffLineHeight = document.getElementById("staff").offsetTop + document.getElementById("staff").offsetHeight;
     var noteOrder = ["C", "D", "E", "F", "G", "A", "B"];
     var noteToPlay = ["A", 3];
-    var notesToLearn = ["A3", "B3", "E4", "F#3", "G#3", "C#4", "E#4"];
     var noteHeight = document.getElementsByClassName("note")[0].offsetHeight;
     var headHeight = document.getElementsByClassName("notehead")[0].offsetHeight;
     var staffHeight = document.getElementById("staff").offsetHeight;
