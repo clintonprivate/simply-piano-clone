c5781d802d3ea464cfa96b54d25fdb1066b0740b
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2024-07-19 04:33:31 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function setInstructionText(note) {
-    document.getElementById("instruct").innerHTML = "Play the note " + note[0];
-}
-
-setInstructionText(["A", 3]);
-setNotePosition();
-addMoreNotes("newnote");
